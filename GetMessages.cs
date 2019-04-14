b5f0128e2d33b9643058b4989cd6c0efc7f62dca
Tyler Smith <Tyler.Smith@svu.edu>
2019-04-13 22:52:25 -0400

    FIXED THE THING!!!!!

--- GetMessages.cs
+++ GetMessages.cs
@@ -12,6 +12,7 @@ using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.Documents.Linq;
 using static Vicinia.sharedObjects;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Vicinia
 {
@@ -38,28 +39,46 @@ namespace Vicinia
             var longitude = locationObject.GetValue("long");
 
             var query = _client.CreateDocumentQuery(UriFactory.CreateDocumentCollectionUri("MessageDB", "Messages"),
-                CreateQuery(new Location(Convert.ToDouble(longitude), Convert.ToDouble(latitude)), time)).AsDocumentQuery();
+                CreateQuery(new Location(Convert.ToDouble(longitude), Convert.ToDouble(latitude)), time),
+                new FeedOptions { MaxItemCount = -1 }).AsDocumentQuery();
 
-            var results = new List<object>();
+            var results = GetPostsAsync((p => p != null), CreateQuery(new Location(Convert.ToDouble(longitude), Convert.ToDouble(latitude)), time));//new List<object>();
 
-            while (query.HasMoreResults)
-            {
-                results.Add(await query.ExecuteNextAsync());
-            }
+            // while (query.HasMoreResults)
+            // {
+            //     results.Add(await query.ExecuteNextAsync());
+            // }
 
             return results != null
-                ? (ActionResult)new OkObjectResult($"Hello, {results}")
+                ? (ActionResult)new OkObjectResult($"{JArray.Parse(JsonConvert.SerializeObject(results.Result))}")
                 : new BadRequestObjectResult("Please pass a location on the query string or in the request body");
         }
 
         public static string CreateQuery(Location location, DateTime time)
         {
             var radius = .0003;
+            var testTime = time.AddDays(-1).ToString("yyyy-MM-ddTHH:mm:ssZ");
             var query = $"SELECT value c.id FROM c where (c.location.lat between {location.Latitiude - radius} and {location.Latitiude + radius}) " +
             $"and (c.location.long between {location.Longitude - radius} and {location.Longitude + radius}) " + 
-            $"and (c.time between {time.ToString("yyyy-MM-dd-HH-mm-ss")} and {time.AddDays(-1).ToString("yyyy-MM-dd-HH-mm-ss")}) " + 
+            $"and (c.time >= \"{testTime}\") " + 
             " order by c.time desc";
             return query;
+        }
+
+        public static async Task<IEnumerable<string>> GetPostsAsync(Expression<Func<string, bool>> predicate, string queryString)
+        {
+            IDocumentQuery<string> query = _client.CreateDocumentQuery<string>(
+                UriFactory.CreateDocumentCollectionUri("MessageDB", "Messages"), queryString,
+                new FeedOptions { MaxItemCount = -1 })
+                .AsDocumentQuery();
+
+            List<string> results = new List<string>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<string>());
+            }
+
+            return results;
         }
     }
 }
